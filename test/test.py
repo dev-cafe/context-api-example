9da90ff10b79156d0ad3338905bd51f64682705b
Radovan Bast <bast@users.noreply.github.com>
2016-05-04 22:10:44 +0200

    add test sources

---
+++ test/test.py
@@ -0,0 +1,23 @@
+def _example(lib):
+    account1 = lib.example_new()
+
+    lib.example_deposit(account1, 100.0)
+    lib.example_deposit(account1, 100.0)
+
+    account2 = lib.example_new()
+
+    lib.example_deposit(account2, 200.0)
+    lib.example_deposit(account2, 200.0)
+
+    lib.example_withdraw(account1, 50.0)
+
+    assert lib.example_get_balance(account1) == 150.0
+    assert lib.example_get_balance(account2) == 400.0
+
+    lib.example_free(account1)
+    lib.example_free(account2)
+
+
+def test_fortran():
+    from example import f_lib
+    _example(f_lib)
