a028dcb38cb5fd12e6d666ecff5e8006188d91e3
Radovan Bast <bast@users.noreply.github.com>
2016-05-05 12:37:49 +0200

    add a c++ test

---
+++ test/test.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <cstdio>
+
+#include "example.h"
+
+
+int main()
+{
+    example_context_t *account1 = example_new();
+
+    example_deposit(account1, 100.0);
+    example_deposit(account1, 100.0);
+
+    example_context_t *account2 = example_new();
+
+    example_deposit(account2, 200.0);
+    example_deposit(account2, 200.0);
+
+    example_withdraw(account1, 50.0);
+
+    assert (example_get_balance(account1) == 150.0);
+    assert (example_get_balance(account2) == 400.0);
+
+    example_free(account1);
+    example_free(account2);
+
+    printf("test passed\n");
+
+    return 0;
+}
