cc21615e62d80a5b1409ff14076c61c22b20c0a7
Radovan Bast <bast@users.noreply.github.com>
2017-05-23 20:09:31 +0200

    outsource boilerplate code

--- api/example.py
+++ api/example.py
@@ -1,7 +1,6 @@
-import sys
+from cffi_helpers import get_lib_handle
 import os
-from subprocess import Popen, PIPE
-from cffi import FFI
+import sys
 
 
 def get_env(v):
@@ -10,25 +9,6 @@ def get_env(v):
         sys.stderr.write('Error: Environment variable {0} is undefined\n'.format(v))
         sys.exit(1)
     return _v
-
-
-def get_library_suffix():
-    if sys.platform == "darwin":
-        return 'dylib'
-    else:
-        return 'so'
-
-
-def get_lib_handle(definitions, header, library, build_dir, include_dir):
-    ffi = FFI()
-
-    interface = Popen(['cc', '-E'] + definitions + [os.path.join(include_dir, header)],
-                      stdout=PIPE).communicate()[0].decode("utf-8")
-    ffi.cdef(interface)
-
-    suffix = get_library_suffix()
-    lib = ffi.dlopen(os.path.join(build_dir, 'lib{0}.{1}'.format(library, suffix)))
-    return lib
 
 
 build_dir = get_env('PROJECT_BUILD_DIR')
