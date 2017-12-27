4cc8cba3e43374cb66d18fc86fc0c05dbfdd830c
Radovan Bast <bast@users.noreply.github.com>
2017-12-27 21:12:32 +0100

    add setup.py script

--- account/__init__.py
+++ account/__init__.py
@@ -10,10 +10,15 @@ def get_env(v):
         sys.exit(1)
     return _v
 
-
-_library_dir = os.path.join(get_env('ACCOUNT_BUILD_DIR'), 'lib')
 _this_path = os.path.dirname(os.path.realpath(__file__))
-_include_dir = _this_path
+
+_library_dir = os.getenv('ACCOUNT_LIBRARY_DIR')
+if _library_dir is None:
+    _library_dir = os.path.join(_this_path, 'lib')
+
+_include_dir = os.getenv('ACCOUNT_INCLUDE_DIR')
+if _include_dir is None:
+    _include_dir = os.path.join(_this_path, 'include')
 
 c_lib = get_lib_handle(['-DACCOUNT_API=', '-DACCOUNT_NOINCLUDE'],
                        'account.h',
