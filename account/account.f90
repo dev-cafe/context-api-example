927b148f21ddbef332131709cec1d45af26a156a
Radovan Bast <bast@users.noreply.github.com>
2017-12-27 13:18:22 +0100

    bugfix: is_initialized cannot be module-private
    has to be type-private

--- account/account.f90
+++ account/account.f90
@@ -15,10 +15,8 @@ module example
     type :: account
         private
         real(c_double) :: balance
-        ! it is possible to have allocatable arrays here
+        logical :: is_initialized = .false.
     end type
-
-    logical :: is_initialized = .false.
 
 contains
 
@@ -31,7 +29,7 @@ contains
         context = c_loc(f_context)
         example_new = context
         f_context%balance = 0.0d0
-        is_initialized = .true.
+        f_context%is_initialized = .true.
     end function
 
     subroutine example_free(context) bind (c)
@@ -41,34 +39,42 @@ contains
 
         call c_f_pointer(context, f_context)
         f_context%balance = 0.0d0
+        f_context%is_initialized = .false.
         deallocate(f_context)
-        is_initialized = .false.
     end subroutine
 
-    subroutine stop_if_uninitialized()
-        if (.not. is_initialized) then
+    subroutine check_valid_context(f_context)
+        type(account), pointer, intent(in) :: f_context
+        if (.not. associated(f_context)) then
+            print *, 'ERROR: context is not associated'
+            stop 1
+        end if
+        if (.not. f_context%is_initialized) then
             print *, 'ERROR: context is not initialized'
             stop 1
         end if
     end subroutine
 
-    subroutine example_deposit(context, f) bind (c)
+    subroutine example_withdraw(context, amount) bind (c)
         use, intrinsic :: iso_c_binding, only: c_f_pointer
         type(c_ptr), value :: context
-        real(c_double), value :: f
+        real(c_double), value :: amount
         type(account), pointer :: f_context
 
-        call stop_if_uninitialized()
         call c_f_pointer(context, f_context)
-        f_context%balance = f_context%balance + f
+        call check_valid_context(f_context)
+        f_context%balance = f_context%balance - amount
     end subroutine
 
-    subroutine example_withdraw(context, f) bind (c)
+    subroutine example_deposit(context, amount) bind (c)
+        use, intrinsic :: iso_c_binding, only: c_f_pointer
         type(c_ptr), value :: context
-        real(c_double), value :: f
+        real(c_double), value :: amount
+        type(account), pointer :: f_context
 
-        call stop_if_uninitialized()
-        call example_deposit(context, -f)
+        call c_f_pointer(context, f_context)
+        call check_valid_context(f_context)
+        f_context%balance = f_context%balance + amount
     end subroutine
 
     real(c_double) function example_get_balance(context) bind (c)
@@ -76,8 +82,8 @@ contains
         type(c_ptr), value, intent(in) :: context
         type(account), pointer :: f_context
 
-        call stop_if_uninitialized()
         call c_f_pointer(context, f_context)
+        call check_valid_context(f_context)
         example_get_balance = f_context%balance
     end function
 
