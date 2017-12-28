a1fe5914355fe767a1be8564e506c472a7959aa5
Radovan Bast <bast@users.noreply.github.com>
2017-12-28 10:47:25 +0100

    clang-format

--- account/implementation/cpp_implementation.cpp
+++ account/implementation/cpp_implementation.cpp
@@ -1,23 +1,11 @@
 #include "cpp_implementation.h"
 
-Account::Account()
-{
-    balance = 0.0;
-}
+Account::Account() { balance = 0.0; }
 
 Account::~Account() {}
 
-void Account::deposit(const double amount)
-{
-    balance += amount;
-}
+void Account::deposit(const double amount) { balance += amount; }
 
-void Account::withdraw(const double amount)
-{
-    balance -= amount;
-}
+void Account::withdraw(const double amount) { balance -= amount; }
 
-double Account::get_balance() const
-{
-    return balance;
-}
+double Account::get_balance() const { return balance; }
